ce9efaf036eb16a36924ea5ee98bb5a63cce890f
benysartono <sartonobeny@yahoo.com>
2025-09-02 14:42:19 +0700

    20250902 - 1

--- core/src/main/java/com/beny/libgdx3d1/PojoNetworkUpdate.java
+++ core/src/main/java/com/beny/libgdx3d1/PojoNetworkUpdate.java
@@ -1,9 +1,63 @@
 package com.beny.libgdx3d1;
 
+import java.util.UUID;
+import com.badlogic.gdx.math.Vector3;
+
+
 public class PojoNetworkUpdate {
-	String rot;
-	String rotSpeed; 
-	String ani; 
-	String pos;
+	private UUID uuid;
+	private String actType;
+	private Vector3 rot;
+	private Float rotSpeed; 
+	private String ani; 
+	private Vector3 pos;
+	private Vector3 heading;
+	
+	public UUID getUuid() {
+		return this.uuid;
+	}
+	public String getActType() {
+		return this.actType;
+	}
+	
+	
+	public Vector3 getRot() {
+		return this.rot;
+	}
+	public Float getRotSpeed() {
+		return this.rotSpeed;
+	}
+	public String getAni() {
+		return this.ani;
+	}
+	public Vector3 getPos() {
+		return this.pos;
+	}
+	public Vector3 getHeading() {
+		return this.heading;
+	}
+
+	public void setRot(Vector3 rot) {
+		this.rot = rot;
+	}
+	public void setRotSpeed(Float rotSpeed) {
+		this.rotSpeed = rotSpeed;
+	}
+	public void setAni(String ani) {
+		this.ani = ani;
+	}
+	public void setPos(Vector3 pos) {
+		this.pos = pos;
+	}
+	public void setHeading(Vector3 heading) {
+		this.heading = heading;
+	}
+	public void setUuid(UUID uuid) {
+		this.uuid = uuid;
+	}
+	public void setActType(String actType) {
+		this.actType = actType;
+	}
+
 	
 }
